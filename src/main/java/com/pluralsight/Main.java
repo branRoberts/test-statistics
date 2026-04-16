b11fd352c1f051b0a46c5755b0d6789758b4dfca
branRoberts <brandonkrob12@gmail.com>
2026-04-16 15:24:14 -0400

    Second Commit Made a for loop to find max

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+
+        int testScores [] = {23,45,81,92,100,76};
+
+        int max =  testScores[0];
+        int min = testScores[0];
+// this for loop goes through the entire arrary and compares whether the number thats in the array is greater than the max
+        for(int i = 0; i < testScores.length; i++){
+            if(testScores[i] > max){
+                max = testScores[i];
+            }
+        }
+        System.out.println("Maximum score is " + max);
+    }
 }
