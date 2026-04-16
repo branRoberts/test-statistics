c2ed5be045c8eda7b8e16ab409be783f98a90d3c
branRoberts <brandonkrob12@gmail.com>
2026-04-16 15:44:02 -0400

    Third Commit Made a found the sum and put in the for loop also made code for the average

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
         int max =  testScores[0];
         int min = testScores[0];
+        int avgScore = 0;
+        int sum = 0;
 // this for loop goes through the entire array and compares whether the number that's in the array is greater than the max
         for(int i = 0; i < testScores.length; i++){
             if(testScores[i] > max){
@@ -17,8 +19,13 @@ public class Main {
             if(testScores[i] < min){
                 min = testScores[i];
             }
+            sum += testScores[i];
+
         }
+        avgScore = sum/testScores.length;
         System.out.println("Maximum score is " + max);
         System.out.println("Minimum score is " + min);
+        System.out.println("Average score is " + avgScore);
+
     }
 }
